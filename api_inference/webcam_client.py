b2fcd81bb4b6488b47bb5f2468769a745e311b43
ChloeY <ytyuenab@connect.ust.hk>
2025-10-10 13:31:42 +0800

    update api endpoint to fiona pc

--- api_inference/webcam_client.py
+++ api_inference/webcam_client.py
@@ -16,7 +16,7 @@ import argparse
 
 # Default configuration - modify these values as needed
 DEFAULT_FPS = 10                                    # Frames per second to send to API
-DEFAULT_ENDPOINT = "http://100.80.197.71:8080"      # Model API endpoint
+DEFAULT_ENDPOINT = "http://100.88.240.42:5000"      # Model API endpoint
 DEFAULT_INPUT_SOURCE = 2                            # Webcam index (0 = USB camera, 2 = glasses camera)
 
 # Network configuration
